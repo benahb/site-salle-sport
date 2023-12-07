463ee69e6098db4847ccd7cf2308aa5320ca157e
benahb <tart3170@gmail.com>
2023-12-07 23:50:07 +0100

    creation de la page tarifs

---
+++ resources/views/cours.blade.php
@@ -0,0 +1,66 @@
+@extends('base')
+
+@section('title' , 'cours')
+
+@section('content')
+<section class="cours" id="cour">
+        <div class="titre blanc">
+            <h2 class="titre-text">Nos<span> C</span>ours</h2>
+            
+        </div>
+        <div class="contenu">
+            <div class="box">
+                <div class="imbox">
+                    <img src="img\p7.jpg" alt="">
+                </DIV>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, eum velit, labore quibusdam
+                        molestiae facere rem quaerat veniam minima adipisci beatae unde .</p>
+                    <h3>adhesion annuelle  14,99euro</h3>
+                </div>
+            </div>
+            <div class="box"> 
+                <div class="imbox">
+                    <img src="img\p13.jpg" alt="">
+                </DIV>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, eum velit, labore quibusdam
+                        molestiae facere rem quaerat veniam minima adipisci beatae unde .</p>
+                    <h3>la séance 9,99 euro</h3>
+                </div>
+            </div>
+            <div class="box">
+                <div class="imbox">
+                    <img src="img\p11.jpg" alt="">
+                </DIV>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, eum velit, labore quibusdam
+                        molestiae facere rem quaerat veniam minima adipisci beatae unde .</p>
+                    <h3>le carnet de 10 séances 89,99 euro</h3>
+                </div>
+            </div>
+            <div class="box">
+                <div class="imbox">
+                    <img src="img\p5.jpg" alt="">
+                </DIV>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, eum velit, labore quibusdam
+                        molestiae facere rem quaerat veniam minima adipisci beatae unde .</p>
+                    <h3>abonnement mensuel illimité sans cour 29,99 euro</h3>
+                </div>
+            </div>
+            <div class="box">
+                <div class="imbox">
+                    <img src="img\p9.jpg" alt="">
+                </DIV>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, eum velit, labore quibusdam
+                        molestiae facere rem quaerat veniam minima adipisci beatae unde .</p>
+                    <h3>Abonnement mensuelle illimité avec cour 59,99 euro</h3>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+    </section>
+@endsection
