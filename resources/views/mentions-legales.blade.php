ff11b8f12b80c4bf487bbfa6161511168f7fa2e7
benahb <tart3170@gmail.com>
2023-12-08 17:25:36 +0100

    page d'accueil finalisée

--- resources/views/mentions-legales.blade.php
+++ resources/views/mentions-legales.blade.php
@@ -1,20 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         <title>Fitown-mentions legales</title>
-</head>
-<body>
-<section>
-    <div>
+@extends('base')
+
+@section('title' , 'cours')
+
+@section('content')
+<section class="mentions">
+    <div class="legales">
             <h2>Mentions Legales</h2>
             <p>
-            Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique,<br> il est précisé aux utilisateurs du site fitown l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.  
+            Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique,<br> il est précisé aux utilisateurs du site fitown l'identité des différents intervenants dans le cadre de sa réalisation et sde son suivi.  
             </p>
             <h2>Edition du site</h2>
+            <p> Le présent site, accessible à l’URL www.fitown.fr (le « Site »), est édité par:
+            <br>pierre stephane, résidant 5 bd loui xiv 59000 lille, de nationalité Française (France), né(e) le 14/02/2000, </p>
+            
+            <h2>Hébergement</h2>
+            <p>Le Site est hébergé par la société Hostinger International LTD, situé , 
+               <br> (contact téléphonique ou email : https://www.hostinger.fr/contact). </p>
+
+               <h2>Directeur de publication</h2>
+               <p>Le Directeur de la publication du Site est pierre stephane. </p>
+
+               <h2>Nous contacter</h2>
+               <p>
+               Par téléphone : +33751634463 <br>
+               Par email : contact28677@gmail.com <br>
+               Par courrier : 5 bd loui xiv 59000 lille
+               </p>
+
+               <h2>Données personnelles</h2>
+               <p>Le traitement de vos données à caractère personnel est régi par notre Charte <br>du respect de la vie privée, disponible depuis la section "Charte de <br>Protection des Données Personnelles", conformément au Règlement Général sur la Protection des Données <br>2016/679 du 27 avril 2016 («RGPD»).</p>
 
     </div>
 </section>
-</body>
-</html>
+@endsection
