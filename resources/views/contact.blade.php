722fe7923fcb12dfc1620f960ca95c8703022f4d
benahb <tart3170@gmail.com>
2023-12-09 15:52:29 +0100

    modif sur la capitalisation des lettre page contaact accueil et base

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -28,9 +28,9 @@
      </div>
     <div class="cordonnee">
             <h2>Nos Cordonnées</h2>
-            <p>téléphone : le 06 00 00 00 00</p>
-            <p>adresse : 5 Bd Louis XIV, 59800 Lille</p>
-            <p>support@fitown.com</p>
+            <p>Téléphone : le 06 00 00 00 00</p>
+            <p>Adresse : 5 Bd Louis XIV, 59800 Lille</p>
+            <p>Email : support@fitown.com</p>
         <div class="google">
 
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.9155085751745!2d3.0678293758054056!3d50.62868637451531!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2d58d87da5f2d%3A0x739db98176c2b59d!2s5%20Bd%20Louis%20XIV%2C%2059800%20Lille!5e0!3m2!1sfr!2sfr!4v1702054575326!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
